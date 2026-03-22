39759c4fdb4a270326a85c881b3e540f80c1f00b
shivraj-chavan <shivrajchavan1902@gmail.com>
2026-03-22 08:01:14 +0000

    chnges2

--- app/admin/(panel)/blogs/page.jsx
+++ app/admin/(panel)/blogs/page.jsx
@@ -67,7 +67,7 @@ export default function BlogsPage() {
 
   const generateUniqueSlug = (title) => {
     const base = title.toLowerCase().replace(/[^a-z0-9]+/g, '-').replace(/(^-|-$)/g, '');
-    return `${base}-${Date.now().toString(36)}`;
+    return `${base}`;
   };
 
   const handleInputChange = (e) => {
@@ -135,7 +135,7 @@ export default function BlogsPage() {
       imagePreview: blog.image || '',
       content: blog.content
     });
-    toast.success('Blog edited Successfully !')
+    // toast.success('Blog edited Successfully !')
     setShowModal(true);
   };
 
@@ -390,7 +390,7 @@ export default function BlogsPage() {
       {/* Close Button */}
       <button
         onClick={() => setSelectedBlog(null)}
-        className="absolute top-4 right-4 text-gray-500 hover:text-gray-200 font-extrabold"
+        className="absolute top-4 right-4 text-black cursor-pointer bg-white px-2 py-1 rounded-full font-extrabold"
       >
         ✕
       </button>
